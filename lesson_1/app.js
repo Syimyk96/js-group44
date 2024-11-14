993fc5e4a4d8429dc7d7dd421c4d3a875aeacef0
Syimyk96 <mambetovsyimyk00it@gmail.com>
2024-11-14 21:18:33 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+//alert("Hello JavaScript"); 
+document.write("Hello group 44");
+
+//v8
+        
+document.body.style.color= "white";
+document.body.fontSize = "6rem";
+document.body.style.fontFamily = "Arial, Helvetica, sant-serif";
+        
